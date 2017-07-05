34f09e97a2006fdd53be1eb7ed7cf4f4aebfaac3
HuyAnh <huyanhhb@yahoo.com.vn>
2017-07-05 10:00:43 +0700

    ok

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,61 @@
 'use strict';
 
-angular.module('confusionApp', ['ngRoute'])
-.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider
-            // route for the contactus page
-            .when('/contactus', {
-                templateUrl : 'contactus.html',
-                controller  : 'ContactController'
-            })
-            // route for the menu page
-            .when('/menu', {
-                templateUrl : 'menu.html',
-                controller  : 'MenuController'
-            })
-            // route for the dish details page
-            .when('/menu/:id', {
-                templateUrl : 'dishdetail.html',
-                controller  : 'DishDetailController'
-            })
-            .otherwise('/contactus');
-    
-}]);
+angular.module('confusionApp', ['ui.router'])
+.config(
+  function($stateProvider,$urlRouterProvider) {
+    $stateProvider
+         .
+         state('app',{
+            url:'/',
+            views:{
+                'header':{
+                    templateUrl:'views/header.html'
+                },
+                'content':{
+                    template:'<h1>To be Completed</h1>',
+                    controller:'IndexController'
+                },
+                'footer':{
+                    templateUrl:'views/footer.html'
+                }
+            }
+         })
+         .state('app.aboutus',{
+            url:'aboutus',
+            views:{
+                'content@':{
+                   template:'<h1>To be Completed</h1>'
+                }
+            }
+         })
+         .state('app.contactus',{
+            url:'contactus',
+            views:{
+                'content@':{
+                    templateUrl:'views/contactus.html',
+                    controller:'ContactController'
+                }
+            }
+         })
+          .state('app.menu',{
+            url:'menu',
+            views:{
+                'content@':{
+                    templateUrl:'views/menu.html',
+                    controller:'MenuController'
+                }
+            }
+          })
+          .state('app.dishdetails',{
+            url:'menu/:id',
+            views:{
+                'content@':{
+                    templateUrl:'views/dishdetail.html',
+                    controller:'DishDetailController'
+                }
+            }
+          });
+          $urlRouterProvider.otherwise('/');
+           
+
+});
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -67,13 +67,15 @@ angular.module('confusionApp')
             };
         }])
 
-        .controller('DishDetailController', ['$scope','$routeParams','menuFactory', function($scope,$routeParams,menuFactory) {
-            var dish = menuFactory.getDish(parseInt($routeParams.id,10));
+        .controller('DishDetailController', ['$scope','$stateParams','menuFactory', function($scope,$stateParams,menuFactory) {
+            var dish = menuFactory.getDish(parseInt($stateParams.id,10));
             $scope.dish = dish;
             
             
         }])
-
+        .controller('IndexController',['$scope',function($scope){
+            
+        }])
         .controller('DishCommentController', ['$scope', function($scope) {
             
             //Step 1: Create a JavaScript object to hold the comment from the form
